fd3bcfd408d6bd352a795924bd73409aa69eab1d
RF Liang <shootingstarlrf@hotmail.com>
2020-07-07 20:54:04 +0800

    Update scripts.js

--- SJTUPass/SJTUPass_files/scripts.js
+++ SJTUPass/SJTUPass_files/scripts.js
@@ -96,9 +96,9 @@ function stateTrans() {
         d.style.display = ""
         dt.innerHTML = "需在30分钟内进校，确定进校吗？"
     }
-    else if (s.innerHTML == "进校") {
-        getOutSchool(s, s_en, b, d, dt, n);
-    }
+//    else if (s.innerHTML == "进校") {
+//        getOutSchool(s, s_en, b, d, dt, n);
+//    }
 }
 function confirm() {
     document.getElementById("dialog").style.display = 'none';
